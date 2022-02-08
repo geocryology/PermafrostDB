037d4ee173679b77f9b4f8f0890cf7ec5e55bdcf
nicholas512 <15223327+nicholas512@users.noreply.github.com>
2022-02-08 16:26:47 -0700

    split repair/service functions into 3 files

---
+++ R/dbpf_service_resolve.R
@@ -0,0 +1,46 @@
+
+dbpf_service_resolve <- function(con, id, time_UTC, mode='test'){
+  # check if id exists
+  needs <- 
+    paste0("SELECT obs.id as service_need,
+                   locations.name as loc
+              FROM (SELECT id, location
+                      FROM observations 
+                     WHERE sensor_id = '",service_required_id(con),"') 
+                   AS obs
+                   LEFT JOIN locations
+                   ON locations.coordinates = obs.location
+             WHERE obs.id='",id,"'
+            ")
+  needs <- dbGetQuery(con, needs)
+  
+  completed <- 
+    paste0("SELECT observations.text_value as service_id,
+                   corrected_utc_time
+              FROM observations 
+             WHERE sensor_id = '",service_completed_id(con),"'
+            ")
+  completed <- dbGetQuery(con, completed)
+  
+  if (!(id %in% needs$service_need)){
+    stop("No service record with that ID")
+  }
+  
+  if (id %in% completed$service_id){
+    stop(paste("Service need already resolved", completed$corrected_utc_time))
+  }
+  
+  res <- dbpf_manual_obs_add(con, 
+                             sensor_label = 'service_completed',
+                             time_UTC = time_UTC, 
+                             location_name = needs$loc, 
+                             text_value = id,
+                             height_min_metres = 0,
+                             height_max_metres = 0, 
+                             mode = mode, 
+                             numeric_value = 0
+  )
+  
+  return(res)
+  
+}
