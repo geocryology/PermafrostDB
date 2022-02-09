f72ff9a4094f4d61433a7b553a13ff4c07ef2620
nicholas512 <15223327+nicholas512@users.noreply.github.com>
2022-02-09 16:30:40 -0700

    add horizon angles to database

--- R/id_lookups.R
+++ R/id_lookups.R
@@ -24,3 +24,29 @@ human_observation_id <- function(con){
                FROM devices
               WHERE device_type='human observation'")
 }
+
+
+horizon_angles_id <- function(con, azimuth){
+  
+  if (missing(azimuth)){
+    azimuth <- seq(0, 359, 1)
+  }
+  
+  azimuth <- sprintf("%.3i", as.numeric(unique(azimuth)))
+  labels <- paste0("horizon_angle_at_",azimuth,"_true")
+  
+  sensors <- dbGetQuery(con, paste0("
+                               SELECT id, label
+                                 FROM sensors
+                                WHERE sensors.label IN ('", paste(labels, collapse="','"), "')"))
+  
+  if (length(azimuth) < length(sensors$id)){
+    stop("duplicate sensors detected")
+  }
+  
+  sorted_sensors <- sensors[match(labels, sensors$label),]
+  
+  return(sorted_sensors)
+  
+}
+
