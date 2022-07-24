38c49887f788c99cdc4d03b33645a1195d7dccd6
Han Mac <hannah.macdonell1@gmail.com>
2022-07-24 18:05:30 +0000

    sensor_add upgraded testing

--- R/dbpf_sensor_add.R
+++ R/dbpf_sensor_add.R
@@ -2,15 +2,14 @@
 #'
 #' @title Adds a new sensor to DB
 #'
-#' @description Adds a new sensor to DB and tests for existence of device_id
-#'              and possible duplicate sensor lable.
+#' @description Adds a new sensor to DB and tests for possible duplicate
+#'              sensor lable.
 #'
 #' @details Run in test mode first. If you have no DB login to write data, run
-#'          in test mode with your login and then pass to someone who does.
+#'          in test mode with your login and then pass to someone who does. Note
+#'          a sensor_id must be linked to a device_id using devices_sensors_add.
 #'
 #' @param con Database connection object, as returned by \code{\link{dbpf_con}}
-#' 
-#' @param device_id Charcter string of device that this is linked to.
 #'
 #' @param label Name for the new sensor.
 #'
@@ -24,9 +23,10 @@
 #'
 #' @param precision Precision (absolute) given in the units used.
 #'
-#' @param height_in_metres Positive number above ground, negative below ground.
+#' @param height_in_metres Positive number above ground, negative below ground. 
 #'
-#' @param mode = Can be 'test' (default, read-only) or 'insert' (will insert into DB)
+#' @param mode = Can be 'test' (default, read-only) or
+#'                      'insert' (will insert into DB)
 #'
 #' @return Test results as well as sensor id if data was inserted.
 #'
@@ -34,7 +34,7 @@
 #' @examples
 #' \dontrun{
 #' con <- dbpf_con()
-#' dbpf_sensor_add(con, "5265f740-8e09-4d41-b5b6-2136f5d35ea3",
+#' dbpf_sensor_add(con,
 #'                 "vegetation_height_max", type_of_measurement = "numeric",
 #'                 unit_of_measurement = "m", accuracy = 0.5, precision=0.01,
 #'                 height_in_metres = 0, serial_number = "", mode = 'test')
@@ -42,8 +42,8 @@
 #' }
 #' @author Stephan Gruber <stephan.gruber@@carleton.ca>
 # =============================================================================
- 
-dbpf_sensor_add <- function(con, device_id, label, type_of_measurement,
+
+dbpf_sensor_add <- function(con, label, type_of_measurement,
                             unit_of_measurement, accuracy, precision,
                             height_in_metres = 0, serial_number = "",
                             mode = "test") {
@@ -53,19 +53,38 @@ dbpf_sensor_add <- function(con, device_id, label, type_of_measurement,
     teststring <- "Test result"
     passed <- FALSE
 
-    # check if device id exists
-    query <- paste0("SELECT COUNT(id) FROM devices WHERE id = '",
-            device_id, "'")
-    res <- dbGetQuery(con, query)$count
-    if (res == 0) {
-        teststring <- paste(teststring, "device id not found", sep = ": ")
-    }
-
     #check type_of_measurement
     ok <- (type_of_measurement == "numeric") + (type_of_measurement == "text")
     if (ok == 0) {
         teststring <- paste(teststring, "type_of_measurement must be 'text' ",
                             "or 'numeric'", sep = ": ")
+    }
+
+    #check unit_of_measurement
+    unit_list <- dbGetQuery(con, paste0("SELECT DISTINCT(unit_of_measurement) ",
+                                        "FROM sensors"))
+    if (!unit_of_measurement %in% list(unit_list$unit_of_measurement)[[1]]) {
+        err <- paste0("unit_of_measurement '", unit_of_measurement,
+                        "' doesn't exist in database.")
+        if (!readline(paste0(err, " To add anyways or continue with testing, ",
+                                "type 'Y':")) %in% c("y", "Y")) {
+            teststring <- paste(teststring, err, sep = ": ")
+            print(teststring)
+        }
+    }
+
+    #check height_in_metres
+    height_list <- dbGetQuery(con, paste0("SELECT DISTINCT(height_in_metres) ",
+                                        "FROM sensors"))
+    if (!height_in_metres %in%
+                    list(as.character(height_list$height_in_metres))[[1]]) {
+        err <- paste0("height_in_metres '", height_in_metres,
+                        "' doesn't exist in database.")
+        if (!readline(paste0(err, " To add anyways or continue with testing, ",
+                                "type 'Y':")) %in% c("y", "Y")) {
+            teststring <- paste(teststring, err, sep = ": ")
+            print(teststring)
+        }
     }
 
     if (teststring == "Test result") {
@@ -83,20 +102,10 @@ dbpf_sensor_add <- function(con, device_id, label, type_of_measurement,
                             height_in_metres, serial_number, sep = "', '"),
                             "') RETURNING id")
 
-        try(ins <- dbGetQuery(con, query), silent = TRUE)
+        try(ins <- dbGetQuery(con, query), silent = FALSE)
         if (exists("ins")) {
             teststring <- paste0(teststring, " ==> Sensor added. Resulting ",
             "sensor id: ", ins)
-        }
-        query <- paste0("INSERT INTO devices_sensors (timestamp,
-                            device_id, sensor_id) VALUES ('",
-                            paste(lubridate::now(), device_id, ins,
-                            sep = "', '"), "') RETURNING id")
-
-        try(ins <- dbGetQuery(con, query), silent = TRUE)
-        if (exists("ins")) {
-            teststring <- paste0(teststring, " ==> dev_sen table updated.",
-            "Resulting id: ", ins, "\n")
         }
     }
     return(teststring)
