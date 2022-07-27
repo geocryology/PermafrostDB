a9e6f87b6236d6aa447147b2a99e6b239c45feae
Han Mac <hannah.macdonell1@gmail.com>
2022-07-27 15:25:54 +0000

    Fixed time query bug

--- R/dbpf_device_sensors.R
+++ R/dbpf_device_sensors.R
@@ -28,9 +28,9 @@ dbpf_device_sensors <- function(con, serial_number) {
                 "WHERE serial_number ='", serial_number, "'"))
 
     # Get most recent sensor log timestamp
-    time <- dbGetQuery(con, paste0("SELECT timestamp FROM devices_sensors",
-                " WHERE device_id = '", dev_id, "' GROUP BY ",
-                "devices_sensors.id ORDER BY timestamp DESC LIMIT (1)"))
+    time <- dbGetQuery(con, paste0('SELECT "timestamp" FROM devices_sensors',
+                " WHERE device_id = '", dev_id, "'",
+                ' ORDER BY "timestamp"::date desc LIMIT 1'))
 
     if (nrow(time) < 1){
         print(paste0("Device '", serial_number, "' has no sensors."))
@@ -57,3 +57,4 @@ dbpf_device_sensors <- function(con, serial_number) {
     names(sen_df)[1] <- "sensor_id"
     return(sen_df)
 }
+dbpf_device_sensors(con, 'E53113')
