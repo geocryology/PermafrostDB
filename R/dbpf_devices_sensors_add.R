de85b70fe43646de4e4c6d97063b77476a13deb4
Han Mac <hannah.macdonell1@gmail.com>
2022-07-28 16:04:54 +0000

    Cleaned up with helper functions

--- R/dbpf_devices_sensors_add.R
+++ R/dbpf_devices_sensors_add.R
@@ -17,9 +17,9 @@
 #'          test results.
 #'
 #' @param dev_sen Data frame with these columns (type):
-#'                oldSerial (character)
-#'                newSerial (character)
-#'                comment (character)
+#'                oldSerial: logger being removed from thermistor chain
+#'                newSerial: logger being newly attatched to thermistor chain
+#'                comment
 #'                time (POSIXct, UTC)
 #'
 #' @param mode Can be 'test' (default) or
@@ -54,13 +54,91 @@ dbpf_devices_sensors_add <- function(con, dev_sen, mode = "test") {
     }
 
     # Test information provided
+    dev_sen <- dev_sen_formatting(dev_sen)
+
+    # Loop over rows in table
+    for (r in 1:nrow(dev_sen)) {
+        print(paste0("Moving ", dev_sen$oldSerial[r],
+        " sensors to ", dev_sen$newSerial[r]))
+        # Do all devices exist?
+        dev_sen$old[r] <- (dbpf_device_exists(con, dev_sen$oldSerial[r]) == 1)
+        dev_sen$new[r] <- (dbpf_device_exists(con, dev_sen$newSerial[r]) == 1)
+
+        if (dev_sen$old[r] + dev_sen$new[r] != 2) {
+            dev_sen$result[r] <- "FAILED"
+            dev_sen$message[r] <- "Logger(s) don't not exist."
+        } else {
+
+            # Check to ensure newSerial doesn't have sensors attatched to it
+            if (dbpf_device_sensors(con, dev_sen$newSerial[r]) != 0) {
+                dev_sen$result[r] <- "FAILED"
+                dev_sen$message[r] <- "Logger has existing thermisor chain."
+                next
+            }
+
+            # Get all "#1:oC"-type sensors attatched to old_dev_id
+            sen_ids <- dbpf_device_sensors(con, dev_sen$oldSerial[r])
+            if (nrow(sen_ids) != 0) {
+                sen_ids <- sen_ids[grep("\\#\\d\\:oC", sen_ids$label), ]
+            }
+
+            for (s in 1:nrow(sen_ids)) {
+                # Ensuring all sensors exist in database
+                if (sensor_exists(con, sen_ids$sensor_id[s]) != 1) {
+                    next
+                }
+
+                # Move sensors from old logger to new logger
+                dev_sen <- migrate_sensors(con, dev_sen, r, sen_ids, s, mode)
+
+                # Remove sensors from oldSerial
+                remove_chain(con, dev_sen$time[r],
+                                get_devid(con, dev_sen$oldSerial[r]),
+                                dev_sen$old[r])
+            }
+        }
+    }
+    print(dev_sen[, c("oldSerial", "old", "newSerial",
+                        "new", "result", "message")])
+}
+
+
+migrate_sensors <- function(con, dev_sen, r, sen_ids, s, mode) {
+    query <- paste0("INSERT INTO devices_sensors",
+                    "(timestamp, device_id, sensor_id, notes)",
+                    " VALUES ('",
+                        dev_sen$time[r], "', '",
+                        get_devid(con, dev_sen$newSerial[r]), "', '",
+                        sen_ids$sensor_id[s], "', '",
+                        dev_sen$comment[r], "');")
+
+    if (mode == "test") {
+
+        dev_sen$result[r] <- "PASSED"
+        dev_sen$message[r] <- "All devices exist. Testing Passed."
+
+    } else {
+        try({
+            res <- dbSendQuery(con, query)
+            }, silent = TRUE)
+        try({
+            dbClearResult(res)
+            }, silent = TRUE)
+
+        dev_sen$result[r] <- "PASSED"
+        dev_sen$message[r] <- "Row inserted into DB"
+        }
+    return(dev_sen)
+}
+
+
+dev_sen_formatting <- function(dev_sen) {
     dev_sen <- subset(dev_sen, select = c("oldSerial", "newSerial",
-                            "comment", "sitename", "time"))
+                            "comment", "time"))
 
     # Fix/test column data type, add check columns
     dev_sen$oldSerial <- as.character(dev_sen$oldSerial)
     dev_sen$newSerial <- as.character(dev_sen$newSerial)
-    dev_sen$sitename     <- as.character(dev_sen$sitename)
     dev_sen$comment      <- as.character(dev_sen$comment)
 
     if (lubridate::is.POSIXct(dev_sen$time) == FALSE) {
@@ -81,99 +159,67 @@ dbpf_devices_sensors_add <- function(con, dev_sen, mode = "test") {
     # Check for duplicates (in dev_sen table)
     dev_sen <- unique(dev_sen)
 
-    # Loop over rows in table
-    for (r in 1:nrow(dev_sen)) {
-        # Do all devices exist?
-        dev_sen$old[r] <- (dbpf_device_exists(con, dev_sen$oldSerial[r]) == 1)
-        dev_sen$new[r] <- (dbpf_device_exists(con, dev_sen$newSerial[r]) == 1)
-
-        old_dev_id <- dbGetQuery(con, paste0("SELECT id FROM devices ",
-        "WHERE serial_number ='", dev_sen$oldSerial[r], "'"))
-
-        new_dev_id <- dbGetQuery(con, paste0("SELECT id FROM devices ",
-        "WHERE serial_number ='", dev_sen$newSerial[r], "'"))
-
-        # Removing sensors from loggers put into storage
-        if (dev_sen$newSerial[r] == "strip") {
-            res <- dbpf_remove_sensors(con,
-                                        mode,
-                                        dev_sen$time[r],
-                                        old_dev_id,
-                                        dev_sen$oldSerial[r])
-            dev_sen$result[r] <- res[1]
-            dev_sen$message[r] <- res[2]
-            next
-        }
-
-        # If both old and new loggers exist
-        if (dev_sen$old[r] + dev_sen$new[r] == 2) {
-
-            # Get all "#1:oC"-type sensors attatched to old_dev_id
-            sen_ids <- dbpf_device_sensors(con, dev_sen$oldSerial[r])
-            sen_ids <- sen_ids[grep("\\#\\d\\:oC",sen_ids$label), ]
-
-            # Ensuring all sensors exist in database
-            for (s in 1:nrow(sen_ids)){
-                a <- dbpf_sensor_exists(con, sen_ids$sensor_id[s])
-                if (a != 1){
-                    print(paste0(sen_ids$sensor_id[s], " DOES NOT EXIST."))
-                }
-            }
-
-            for (s in 1:nrow(sen_ids)) {
-                query <- paste0("INSERT INTO devices_sensors",
-                "(timestamp, device_id, sensor_id, notes) VALUES ('",
-                dev_sen$time[r], "', '", new_dev_id, "', '",
-                sen_ids$sensor_id[s], "', '", dev_sen$comment[r], "');")
-                if (mode == "test") {
-                    dev_sen$result[r] <- "PASSED"
-                    dev_sen$message[r] <- "All devices exist. Testing Passed."
-                } else {
-                    try({
-                        res <- dbSendQuery(con, query)
-                        }, silent = TRUE)
-                    try({
-                        dbClearResult(res)
-                        }, silent = TRUE)
-                    dev_sen$result[r] <- "PASSED"
-                    dev_sen$message[r] <- "Row inserted into DB"
-                }
-            }
-        } else {
-            dev_sen$result[r] <- "FAILED"
-            dev_sen$message[r] <- "Logger(s) don't not exist."
-        }
-    }
-    print(dev_sen[, c("oldSerial", "old", "newSerial",
-                        "new", "result", "message")])
+    return(dev_sen)
 }
 
 
-dbpf_sensor_exists <- function(con, sensor_id) {
-    query <- paste0("SELECT COUNT(*) FROM sensors WHERE id = '",
-                   sensor_id, "'")
-    exists <- dbGetQuery(con, query)
-
-    return(exists$count)
-}
-
-dbpf_remove_sensors <- function(con, mode, time, dev_id, serial_number) {
-    if (mode == "test"){
-        n <- nrow(dbpf_device_sensors(con, serial_number))
-        return(c("PASSED", paste0("Logger has ", n, " sensors to strip")))
+sensor_exists <- function(con, sensor_id) {
+    try({
+        res <- dbGetQuery(con, paste0("SELECT COUNT(*) FROM sensors",
+                   " WHERE id = '", sensor_id, "' "))
+        }, silent = TRUE)
+    if (!exists("res")) {
+        return(FALSE)
     } else {
-        # "no_sensors" sen_id
-        sen_id <- "e1f17e20-6ebc-4f9a-8e4a-47995e73ace9"
-        comment <- "No sensors on device."
+        return(res)
+    }
+}
+
+
+remove_chain <- function(con, time, dev_id, serial_number) {
+    # "no_sensors" sen_id
+    comment <- "No thermistor chain on device."
+    sen_ids <- dbpf_device_sensors(con, serial_number)
+    if ("no_sensors" %in% sen_ids$label) {
+        return(c("PASSED", "No sensor chain on this device."))
+    }
+    sen_ids <- sen_ids[grep("\\#HK.*", sen_ids$label), ]
+    if (nrow(sen_ids) > 0) {
+        # Update HK sensors
+        for (row in 1:nrow(sen_ids)){
+            query <- paste0("UPDATE devices_sensors ",
+            "SET timestamp = '", time, "', ",
+            "device_id = '", dev_id, "', ",
+            "sensor_id = '", sen_ids$sensor_id, "', ",
+            "notes = '", comment, "' ",
+            "WHERE sensor_id = '", sen_ids$sensor_id, "';")
+            try({
+                res <- dbSendQuery(con, query)
+                }, silent = FALSE)
+            try({
+                dbClearResult(res)
+                }, silent = TRUE)
+        }
+        return(c("PASSED", "Sensor chain removed for this device."))
+    } else {
+        # Insert no_sensor
         query <- paste0("INSERT INTO devices_sensors (timestamp, ",
                     "device_id, sensor_id, notes) VALUES ('", time,
-                    "', '", dev_id, "', '", sen_id, "', '", comment, "');")
+                    "', '", dev_id, "', '", no_sensor_id(con), "', '",
+                    comment, "');")
         try({
             res <- dbSendQuery(con, query)
-            }, silent = TRUE)
+            }, silent = FALSE)
         try({
             dbClearResult(res)
             }, silent = TRUE)
-        return(c("PASSED", "Row inserted into DB"))
+        return(c("PASSED",  "Sensor chain removed for this device."))
     }
+}
+
+
+get_devid <- function(con, serial_number) {
+    id <- dbGetQuery(con, paste0("SELECT id FROM devices ",
+        "WHERE serial_number ='", serial_number, "'"))
+    return(id)
 }
